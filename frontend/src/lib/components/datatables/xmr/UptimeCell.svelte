a5313bb17d7964d21a718c673e242f97bb449604
ditatompel <ditatompel@gmail.com>
2024-06-11 23:22:00 +0700

    fix: Wrong color display when uptime is 80 & 98
    The UI display red color if the uptime value is exactly
    80 and 98 percent. Use `>=` fix this issue.

--- frontend/src/lib/components/datatables/xmr/UptimeCell.svelte
+++ frontend/src/lib/components/datatables/xmr/UptimeCell.svelte
@@ -2,9 +2,9 @@
 	export let uptime: number;
 </script>
 
-{#if uptime > 98}
+{#if uptime >= 98}
 	<span class="text-green-800 dark:text-green-500">{uptime}%</span>
-{:else if uptime < 98 && uptime > 80}
+{:else if uptime < 98 && uptime >= 80}
 	<span class="text-sky-800 dark:text-sky-500">{uptime}%</span>
 {:else if uptime < 80 && uptime > 75}
 	<span class="text-orange-800 dark:text-orange-300">{uptime}%</span>
