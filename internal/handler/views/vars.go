4dfab11d2c427d50946cba76efc9324d5946dcdb
Christian Ditaputratama <ditatompel@gmail.com>
2024-10-31 16:25:16 +0700

    feat(style)!: Added the new homepage design view
    TODO: Add copy to clipboard functionality

---
+++ internal/handler/views/vars.go
@@ -0,0 +1,16 @@
+package views
+
+type link struct {
+	Text string
+	URI  string
+}
+
+var communityLinks = []link{
+	{Text: "moneroworld.com", URI: "https://moneroworld.com"},
+	{Text: "monero.how", URI: "https://www.monero.how"},
+	{Text: "monero.observer", URI: "https://www.monero.observer"},
+	{Text: "revuo-xmr.com", URI: "https://revuo-xmr.com"},
+	{Text: "themonoeromoon.com", URI: "https://www.themoneromoon.com"},
+	{Text: "monerotopia.com", URI: "https://monerotopia.com"},
+	{Text: "sethforprivacy.com", URI: "https://sethforprivacy.com"},
+}
